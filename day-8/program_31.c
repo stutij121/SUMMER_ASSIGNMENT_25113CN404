83a855018be42b56e61c1c279d4f65a8e68aad91
Stuti Jain <stutij121@gmail.com>
2026-06-08 21:18:40 +0530

    added day 8 programs

---
+++ day-8/program_31.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main ()
+{
+    int i,j,n;
+    char ch ;
+    printf("enter the number of rows");
+    scanf("%d",& n);
+    for(i=1;i<=n;i++)
+    {
+        ch='A';
+        for(j=1;j<=i;j++)
+        {
+            printf("%c",ch);
+            ch++;
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
