2e4255649e389bb6e25adb68e76e6c67c2efe63a
stutij121 <stutij121@gmail.com>
2026-07-02 00:33:05 +0530

    Update 78_symmetric_matrix.c

--- day-20/78_symmetric_matrix.c
+++ day-20/78_symmetric_matrix.c
@@ -3,12 +3,21 @@
 int main()
 {
     int a[50][50];
-    int r, c, i, j,flag=0;
+    int r, c, i, j;
+    int flag = 1;   
 
+    // Input rows and columns
     printf("Enter number of rows and columns: ");
     scanf("%d %d", &r, &c);
 
-    // Input matrix
+    // A symmetric matrix must be square
+    if (r != c)
+    {
+        printf("Matrix is not symmetric because it is not a square matrix.");
+        return 0;
+    }
+
+    // Input matrix elements
     printf("Enter matrix elements:\n");
     for(i = 0; i < r; i++)
     {
@@ -17,22 +26,35 @@ int main()
             scanf("%d", &a[i][j]);
         }
     }
-for(i=0;i<r;i++)
-{
-    for(j=0;j<c;j++)
+
+    // Check if a[i][j] is equal to a[j][i]
+    for(i = 0; i < r; i++)
     {
-        if(a[i][j]!=a[j][i])
+        for(j = 0; j < c; j++)
         {
-            flag=1;
+            if(a[i][j] != a[j][i])
+            {
+                flag = 0;
+                break;
+            }
+        }
+
+        
+        if(flag == 0)
+        {
+            break;
         }
     }
-}
-if(flag==1)
-{
-    printf("symmetric matrix");
-}
-else {
-    printf("not symmentric");
-}
-return 0;
+
+    // Display the result
+    if(flag == 1)
+    {
+        printf("Matrix is symmetric.");
+    }
+    else
+    {
+        printf("Matrix is not symmetric.");
+    }
+
+    return 0;
 }
